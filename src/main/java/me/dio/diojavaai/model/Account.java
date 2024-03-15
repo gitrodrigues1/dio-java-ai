8e807a50f7c681b1fb1d423934419994652871af
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-03-15 15:49:52 -0300

    Add unique constraint to Account and Card number fields

--- src/main/java/me/dio/diojavaai/model/Account.java
+++ src/main/java/me/dio/diojavaai/model/Account.java
@@ -17,6 +17,7 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String number;
 
     private String agency;
--- src/main/java/me/dio/diojavaai/model/Card.java
+++ src/main/java/me/dio/diojavaai/model/Card.java
@@ -17,6 +17,7 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String number;
 
     @Column(name = "avaliable_limit", precision = 13, scale = 2)
