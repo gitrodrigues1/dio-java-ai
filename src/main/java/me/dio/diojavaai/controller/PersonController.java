5e5f953e4fb24d7183817e4c78d8d9073b4252a0
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-03-15 16:12:54 -0300

    Add Swagger annotations to PersonController

--- src/main/java/me/dio/diojavaai/controller/PersonController.java
+++ src/main/java/me/dio/diojavaai/controller/PersonController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import me.dio.diojavaai.model.Person;
 import me.dio.diojavaai.service.IPersonService;
@@ -29,6 +32,11 @@ public class PersonController {
     }
 
     @PostMapping
+    @Operation(summary = "Create a new user", description = "Create a new user and return created user's data.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "User created successfully"),
+        @ApiResponse(responseCode = "404", description = "Invalid user data provided")
+    })
     public ResponseEntity<Person> createPerson(@Valid @RequestBody Person person) {
         var createdPerson = personService.create(person);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest()
